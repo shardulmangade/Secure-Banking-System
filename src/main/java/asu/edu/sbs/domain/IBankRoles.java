2411830c680b21d33c7542378db80fe00e9bfa5d
ShardulMangade <shardul.mangade@gmail.com>
2013-10-26 01:18:15 -0700

    Added IBank role for IT

--- src/main/java/asu/edu/sbs/domain/IBankRoles.java
+++ src/main/java/asu/edu/sbs/domain/IBankRoles.java
@@ -7,4 +7,6 @@ public interface IBankRoles {
 	public final String ROLE_EXTERNAL_MERCHANT = "ROLE_EXTERNAL_MERCHANT";
 	public final String ROLE_HR_EMPLOYEE = "ROLE_HR_EMPLOYEE";
 	public final String ROLE_HR_MANAGER = "ROLE_HR_MANAGER";
+	public final String ROLE_IT_EMPLOYEE = "ROLE_IT_EMPLOYEE";
+	public final String ROLE_IT_MANAGER = "ROLE_IT_MANAGER";
 }
