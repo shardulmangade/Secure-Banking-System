abcce29a9ff6014983444fb4e92a8eb96803e4cb
ramkumarkumaresan <ramk@asu.edu>
2013-10-26 00:59:08 -0700

    Added email notification

--- src/main/java/asu/edu/sbs/email/EmailNotificationManager.java
+++ src/main/java/asu/edu/sbs/email/EmailNotificationManager.java
@@ -5,23 +5,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import asu.edu.sbs.domain.User;
+import asu.edu.sbs.login.service.OneTimePassword;
 
+@Service
 public class EmailNotificationManager{
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(EmailNotificationManager.class);
 	
+	public final static int SUCCESS = 1;
+	public final static int FAILURE = 0;
+	
 	@Autowired
 	private EmailNotificationSender emailSender;
 	
 
-//	public void sendAccountDeactivationEmail(IUser user, String adminid) {
-//		if(user.getEmail()!= null && !user.getEmail().equals(""))
-//		{
-//			emailSender.sendNotificationEmail(user.getEmail(), emailMessages.getProperty("email.account_deactivation_subject"), emailMessages.getProperty("email.account_deactivation_msg"));
-//			logger.info("The admin "+adminid+" sent a deactivation email to "+user.getUserName());
-//		}	
-//	}
-	
-
+	public int sendOTP(User user, OneTimePassword otp)
+	{
+		if(user.getEmail() != null && !user.getEmail().equals(""))
+		{
+			StringBuilder message = new StringBuilder();
+			message.append("Dear User,\n\nYour One Time Password is: "+otp.getPassword()+". Expiration Time: "+otp.getExpirationTime());
+			emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your One Time Password", message.toString());
+			logger.info("An OTP was sent to "+user.getEmail());
+			return SUCCESS;
+		}
+		return FAILURE;
+	}
 }
