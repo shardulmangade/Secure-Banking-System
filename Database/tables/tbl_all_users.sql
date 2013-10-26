29e99685b4d24fcbdf61ad08b1949c1af30f187b
ShardulMangade <shardul.mangade@gmail.com>
2013-10-26 16:12:33 -0700

    Merge commit

--- Database/tables/tbl_all_users.sql
+++ Database/tables/tbl_all_users.sql
@@ -21,3 +21,7 @@ values("admin","21232f297a57a5a743894a0e4a801fc3", null, null, "ROLE_VALID_USER"
 #password is euser
 insert into tbl_all_users 
 values("euser","3960edfad5d748670763a3ded95af414", null, null, "ROLE_VALID_USER","dexter","morgan","ramkumar007@gmail.com","",substring_index(user(),'@',1), curdate());
+
+#password is sysad
+insert into tbl_all_users 
+values("sysad","a3fe41c36274fa31157d64bd152c8eeb", null, null, "ROLE_VALID_USER","Balin","Fundin","shardul27@gmail.com","",substring_index(user(),'@',1), curdate());
