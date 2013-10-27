638764bbb368ed7a53423949f6be47cfe6ecce3a
ramkumarkumaresan <ramk@asu.edu>
2013-10-26 18:44:04 -0700

    Updated controller loads in configuration

--- src/main/java/asu/edu/sbs/exception/BankExceptionHandler.java
+++ src/main/java/asu/edu/sbs/exception/BankExceptionHandler.java
@@ -12,25 +12,14 @@ public class BankExceptionHandler {
 
 	
 	private static final Logger logger = LoggerFactory.getLogger(BankExceptionHandler.class);
-//
-//	
-//	@ExceptionHandler(QuadrigaStorageException.class)
-//	public ModelAndView handleSQLException(QuadrigaStorageException ex) {
+
+	
+
+//	@ExceptionHandler(value ={ BankAccessException.class})
+//	public ModelAndView handleUserAccessException(BankAccessException ex) {
 //		
 //		ModelAndView modelAndView = new ModelAndView();
-//		modelAndView.setViewName("auth/storageissue");
-//		modelAndView.addObject("ex_name", ex.getClass().getName());
-//		modelAndView.addObject("ex_message", ex.getMessage());
-//		logger.error(ex.getMessage(), ex);
-//		return modelAndView;
-//	}
-//	
-//
-//	@ExceptionHandler(value ={ QuadrigaAccessException.class})
-//	public ModelAndView handleUserAccessException(QuadrigaAccessException ex) {
-//		
-//		ModelAndView modelAndView = new ModelAndView();
-//		modelAndView.setViewName("auth/accessissue");
+//		modelAndView.setViewName("exceptions/accessissue");
 //		modelAndView.addObject("ex_name", ex.getClass().getName());
 //		modelAndView.addObject("ex_message", ex.getMessage());
 //		logger.error(ex.getMessage(), ex);
