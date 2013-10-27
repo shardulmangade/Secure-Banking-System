44306cf4a08ca42b495bf467c3238ae442f8b00d
ramkumarkumaresan <ramk@asu.edu>
2013-10-26 17:00:50 -0700

    Revert "Added entry for IT employee"
    This reverts commit 03d58b24b357133c98b819b529fa374ca0a46f07.

--- Database/tables/tbl_user_role.sql
+++ Database/tables/tbl_user_role.sql
@@ -13,6 +13,3 @@ values("admin","ROLE_EXTERNAL_USER",substring_index(user(),'@',1), curdate(),sub
 
 insert into tbl_user_role
 values("euser","ROLE_EXTERNAL_USER",substring_index(user(),'@',1), curdate(),substring_index(user(),'@',1), curdate());
-
-insert into tbl_user_role
-values("sysad","ROLE_EXTERNAL_USER",substring_index(user(),'@',1), curdate(),substring_index(user(),'@',1), curdate());
