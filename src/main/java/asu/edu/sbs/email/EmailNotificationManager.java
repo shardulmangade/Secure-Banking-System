42923b911e80ec7f92169b6b6a71f827de088582
ramkumarkumaresan <ramk@asu.edu>
2013-11-02 14:40:57 -0700

    Changed email to threads. Now no delay in front end

--- src/main/java/asu/edu/sbs/email/EmailNotificationManager.java
+++ src/main/java/asu/edu/sbs/email/EmailNotificationManager.java
@@ -3,9 +3,9 @@ package asu.edu.sbs.email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import asu.edu.sbs.domain.SignUpEmployee;
 import asu.edu.sbs.domain.User;
 import asu.edu.sbs.login.service.OneTimePassword;
 
@@ -18,8 +18,11 @@ public class EmailNotificationManager{
 	public final static int SUCCESS = 1;
 	public final static int FAILURE = 0;
 
+//	@Autowired
+//	private EmailNotificationSender emailSender;
+	
 	@Autowired
-	private EmailNotificationSender emailSender;
+	private JavaMailSender mailSender;
 
 
 	public int sendOTP(User user, OneTimePassword otp)
@@ -29,38 +32,90 @@ public class EmailNotificationManager{
 			StringBuilder message = new StringBuilder();
 			message.append("Dear User,\n\nYour One Time Password is: \""+otp.getPassword()+"\"(without the quotes). Expiration Time: "+otp.getExpirationTime() +" and its valid for only one login.");
 
-			if(emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your One Time Password", message.toString()) == SUCCESS)
-			{
-				logger.info("An OTP was sent to "+user.getEmail());
-				return SUCCESS;
-			}
+			EmailNotificationSender emailSender = new EmailNotificationSender(mailSender, user.getEmail(), "SunDevilBank:Your One Time Password", message.toString());
+			Thread emailThread = new Thread(emailSender);
+			emailThread.start();
+			
+			logger.info("An OTP was sent to "+user.getEmail());
+
 		}
-		return FAILURE;
+		return SUCCESS;
 	}
-	
+
 	public int sendPassword(User user, String password)
 	{
 		if(user.getEmail() != null && !user.getEmail().equals(""))
 		{
 			StringBuilder message = new StringBuilder();
 			message.append("Dear User,\n\nYour temporary password is: "+   password +".\n Please change the password once you log in\n");
-			emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+			
+//			emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+			EmailNotificationSender emailSender = new EmailNotificationSender(mailSender, user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+			Thread emailThread = new Thread(emailSender);
+			emailThread.start();
+			
 			logger.info("Temporary password was sent to "+user.getEmail());
-			return SUCCESS;
-		}
-		return FAILURE;
-	}
 	
+		}
+		return SUCCESS;
+	}
+
 	public int sendPasswordCustomer(User user, String password)
 	{
 		if(user.getEmail() != null && !user.getEmail().equals(""))
 		{
 			StringBuilder message = new StringBuilder();
 			message.append("Dear User,\n\nYour temporary password is: "+   password +".\n Please change the password once you log in\n");
-			emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+			
+//			emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+			EmailNotificationSender emailSender = new EmailNotificationSender(mailSender, user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+			Thread emailThread = new Thread(emailSender);
+			emailThread.start();
 			logger.info("Temporary password was sent to "+user.getEmail());
-			return SUCCESS;
+			
 		}
-		return FAILURE;
+		return SUCCESS;
 	}
+
+	//	public int sendOTP(User user, OneTimePassword otp)
+	//	{
+	//		if(user.getEmail() != null && !user.getEmail().equals(""))
+	//		{
+	//			StringBuilder message = new StringBuilder();
+	//			message.append("Dear User,\n\nYour One Time Password is: \""+otp.getPassword()+"\"(without the quotes). Expiration Time: "+otp.getExpirationTime() +" and its valid for only one login.");
+	//
+	//			if(emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your One Time Password", message.toString()) == SUCCESS)
+	//			{
+	//				logger.info("An OTP was sent to "+user.getEmail());
+	//				return SUCCESS;
+	//			}
+	//		}
+	//		return FAILURE;
+	//	}
+	//	
+	//	public int sendPassword(User user, String password)
+	//	{
+	//		if(user.getEmail() != null && !user.getEmail().equals(""))
+	//		{
+	//			StringBuilder message = new StringBuilder();
+	//			message.append("Dear User,\n\nYour temporary password is: "+   password +".\n Please change the password once you log in\n");
+	//			emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+	//			logger.info("Temporary password was sent to "+user.getEmail());
+	//			return SUCCESS;
+	//		}
+	//		return FAILURE;
+	//	}
+	//	
+	//	public int sendPasswordCustomer(User user, String password)
+	//	{
+	//		if(user.getEmail() != null && !user.getEmail().equals(""))
+	//		{
+	//			StringBuilder message = new StringBuilder();
+	//			message.append("Dear User,\n\nYour temporary password is: "+   password +".\n Please change the password once you log in\n");
+	//			emailSender.sendNotificationEmail(user.getEmail(), "SunDevilBank:Your temporary Password", message.toString());
+	//			logger.info("Temporary password was sent to "+user.getEmail());
+	//			return SUCCESS;
+	//		}
+	//		return FAILURE;
+	//	}
 }
