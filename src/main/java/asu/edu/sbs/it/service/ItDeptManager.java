36795b70a09b1bd290f20bab98c24ee37e5945e5
ShardulMangade <shardul.mangade@gmail.com>
2013-11-02 16:36:57 -0700

    Bug Fixed:Forgot to add a file

--- src/main/java/asu/edu/sbs/it/service/ItDeptManager.java
+++ src/main/java/asu/edu/sbs/it/service/ItDeptManager.java
@@ -4,13 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import asu.edu.sbs.db.ItDBConnectionManager;
+import asu.edu.sbs.db.LoginDBConnectionManager;
 import asu.edu.sbs.domain.SignUpEmployee;
+import asu.edu.sbs.domain.User;
+import asu.edu.sbs.exception.BankAccessException;
+import asu.edu.sbs.exception.BankStorageException;
 
 @Service
 public class ItDeptManager {
 
 	@Autowired
 	private ItDBConnectionManager itdbconnection;
+	@Autowired
+	private LoginDBConnectionManager loginmanager;
 	
 	public void deleteItEmployee(String username) throws Exception
 	{
@@ -22,6 +28,11 @@ public class ItDeptManager {
 		itdbconnection.updateDepartmentOfEmployee(UserName, department);
 	}
 	
+	public int insertValidUser(User user, String password, String createdBy) throws BankStorageException, BankAccessException
+	{		
+			return (loginmanager.insertValidUser(user, password, createdBy));	
+	}	
+	
 	public void insertDeleteRequesttoCM (String userName,String department, boolean deleteApprove) throws Exception 
 	{
 		itdbconnection.insertDeleteRequesttoCM(userName,department,deleteApprove);
@@ -31,5 +42,17 @@ public class ItDeptManager {
 	{
 		return itdbconnection.getDeleteApprovalStatus(userName,department);
 	}
+
+
+	public void updateUserRole(String role,String olddepartment, String department, String username,String changedby) throws BankStorageException, BankAccessException {
+		// TODO Auto-generated method stub
+		loginmanager.updateUserRole(role,olddepartment, department,username ,changedby);
+	}
+
+	public String getRoleTobechanged(String department, String role) {
+		
+		return (loginmanager.getRoleTobechanged( department,  role));
+	}
+	
 	
 }
